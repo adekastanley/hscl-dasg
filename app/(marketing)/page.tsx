77a1423200cbf4ca6197358f1c3f1464f404fbe3
adeka stanley <adekastanley2@gmail.com>
2026-01-08 14:09:31 +0100

    added platform section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,6 +6,7 @@ import StatsSection from "@/components/pages/homepage/Stats";
 
 import { CTASection } from "@/components/pages/homepage/cta";
 import { Extras } from "@/components/pages/homepage/extras";
+import Snapshot from "@/components/pages/homepage/snapshot";
 
 export default function Home() {
 	return (
@@ -14,6 +15,7 @@ export default function Home() {
 			<WhyWeExist />
 			<Services />
 			<Extras />
+			<Snapshot />
 			<StatsSection />
 			<CTASection />
 		</>
