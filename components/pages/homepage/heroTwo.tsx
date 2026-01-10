6db92fe99646ab7052ed0b78b2a22ae84b3f88ad
adeka stanley <adekastanley2@gmail.com>
2026-01-10 13:17:43 +0100

    update

--- components/pages/homepage/heroTwo.tsx
+++ components/pages/homepage/heroTwo.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { MagneticButton } from "@/components/ui/magneticButton";
 
 export default function HeroTwo() {
