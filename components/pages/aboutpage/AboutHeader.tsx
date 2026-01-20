e63a7d4f732b0f4b9faaa3fe145fe94a72cc8fdc
adeka stanley <adekastanley2@gmail.com>
2026-01-20 11:06:44 +0100

    mobile update 02

--- components/pages/aboutpage/AboutHeader.tsx
+++ components/pages/aboutpage/AboutHeader.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 
 export default function AboutHeader() {
 	return (
-		<section className="max-sm:pb-60 w-full relative bg-no-repeat  mx-auto pt-16 md:pt-24 space-y-6 px-6  h-[90dvh] flex items-center min-h-[90dvh]  bg-[url(/assets/ab.jpg)] bg-cover bg-center  mask-[linear-gradient(to_bottom,black_80%,transparent_100%)] ">
+		<section className="max-sm:pb-40 w-full relative bg-no-repeat  mx-auto pt-16 md:pt-24 space-y-6 px-6  h-[90dvh] flex items-center min-h-[90dvh]  bg-[url(/assets/ab.jpg)] bg-cover bg-center  mask-[linear-gradient(to_bottom,black_80%,transparent_100%)] ">
 			{/* <div className="absolute inset-x-0 bottom-0 h-32 bg-linear-to-t from-background  to-transparent" /> */}
 			<section className=" w-full max-w-6xl mx-auto">
 				{/* Top Nav Pill */}
